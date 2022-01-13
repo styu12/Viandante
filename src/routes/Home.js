b0460f8d75b8a7dc7468b5b1a11d09765bcb74ac
Seungoh <styu12@naver.com>
2022-01-13 16:20:00 +0900

    Main Page Layout and Firebase Setting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,4 @@
-import { storageService } from "fbase";
+import { dbService, storageService } from "fbase";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import {
@@ -32,7 +32,10 @@ const FlexRowContainer = styled.div`
   flex-direction: row;
   justify-content: space-between;
   > div {
-    width: 49%;
+    width: 100%;
+  }
+  > div:not(:last-child) {
+    margin-right: 15px;
   }
 `;
 
@@ -47,7 +50,8 @@ const BasicPhotoBox = styled(PhotoBackground)`
 const ContentTitle = styled.p`
   font-size: 18px;
   margin-bottom: 10px;
-  text-align: center;
+  line-height: 1.3;
+  text-align: ${(props) => (props.isCenter ? "center" : "left")};
 `;
 
 // 이미지 밑 진한 제목
@@ -66,25 +70,84 @@ const Banner = styled(PhotoBackground)`
   color: white;
 `;
 
-const Home = () => {
-  // 이미지 담은 state
-  const [photoUrl, setPhotoUrl] = useState("");
+const FlexColumnContainer = styled(FlexRowContainer)`
+  margin: auto;
+  width: 80%;
+  flex-direction: column;
+  > div:not(:last-child) {
+    margin: 0 0 15px 0;
+  }
+`;
 
-  // firestorage에서 이미지 가져오는 함수
-  const getPhotoUrl = async () => {
-    const storageRef = storageService.ref("test.jpeg");
-    const storageUrl = await storageRef.getDownloadURL();
-    setPhotoUrl(storageUrl);
+const ReviewBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const ReviewPhoto = styled(PhotoBackground)`
+  flex: 1;
+  padding-bottom: 33%;
+`;
+
+const ReviewContent = styled.div`
+  flex: 2;
+  padding: 15px 25px;
+`;
+
+const Home = () => {
+  // 관련 데이터 저장한 state들
+  const [stays, setStays] = useState([]);
+  const [magazines, setMagazines] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [BannerPhotos, setBannerPhotos] = useState({});
+
+  // firestore data 모두 불러오기
+  const getData = async () => {
+    // stays data call
+    await dbService.collection("Stays").onSnapshot((snapshot) => {
+      const stayArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setStays(stayArray);
+    });
+
+    // magazines data call
+    await dbService.collection("Magazines").onSnapshot((snapshot) => {
+      const magazineArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMagazines(magazineArray);
+    });
+
+    //review data call
+    await dbService.collection("Reviews").onSnapshot((snapshot) => {
+      const reviewArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setReviews(reviewArray);
+    });
+
+    // main banner data call
+    await dbService.collection("MainBanner").onSnapshot((snapshot) => {
+      const mainBannerArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setBannerPhotos(mainBannerArray[0]);
+    });
   };
 
   // 컴포넌트 렌더링 시 이미지 불러와서 state에 담기
   useEffect(() => {
-    getPhotoUrl();
+    getData();
   }, []);
 
   return (
     <CustomContainer>
-      <TopBanner bg={photoUrl}>
+      <TopBanner bg={BannerPhotos.topBannerUrl}>
         <TopBannerTextBox>
           <TopBannerText>Viandante</TopBannerText>
           <TopBannerText>원주점</TopBannerText>
@@ -94,20 +157,20 @@ const Home = () => {
       <Section>
         <SectionTitle isCenter={true}>Viandante Stay</SectionTitle>
         <FlexRowContainer>
-          <div>
-            <BasicPhotoBox bg={photoUrl}></BasicPhotoBox>
-            <ContentTitle>자연속 힐링 독채 펜션</ContentTitle>
-            <ContentBoldTitle>비안단테 원주</ContentBoldTitle>
-          </div>
-          <div>
-            <BasicPhotoBox bg={photoUrl}></BasicPhotoBox>
-            <ContentTitle>한옥에서의 완벽한 하루</ContentTitle>
-            <ContentBoldTitle>비안단테 춘천</ContentBoldTitle>
-          </div>
+          {/* stays call and rendering */}
+          {stays.map((s) => {
+            return (
+              <div key={s.id}>
+                <BasicPhotoBox bg={s.thumbnailUrl}></BasicPhotoBox>
+                <ContentTitle isCenter={true}>{s.description}</ContentTitle>
+                <ContentBoldTitle isCenter={true}>{s.name}</ContentBoldTitle>
+              </div>
+            );
+          })}
         </FlexRowContainer>
       </Section>
 
-      <Banner bg={photoUrl}>
+      <Banner bg={BannerPhotos.dogBannerUrl}>
         <p>VIANDANTE STAY</p>
         <p>반려견과 함께하는 여행</p>
       </Banner>
@@ -115,16 +178,20 @@ const Home = () => {
       <Section>
         <SectionTitle isCenter={false}>Magazine</SectionTitle>
         <FlexRowContainer>
-          <div>
-            <BasicPhotoBox bg={photoUrl}></BasicPhotoBox>
-            <ContentTitle>"강아지와 차를 안전하게 타는 방법"</ContentTitle>
-            <ContentBoldTitle>작성일자 2022.01.10</ContentBoldTitle>
-          </div>
-          <div>
-            <BasicPhotoBox bg={photoUrl}></BasicPhotoBox>
-            <ContentTitle>"강아지와 물에서 노는 방법"</ContentTitle>
-            <ContentBoldTitle>작성일자 2022.01.01</ContentBoldTitle>
-          </div>
+          {/* magazine data call and rendering */}
+          {magazines.map((m) => {
+            return (
+              <div key={m.id}>
+                <BasicPhotoBox bg={m.thumbnailUrl}></BasicPhotoBox>
+                <ContentTitle isCenter={false}>
+                  {m.title.split("\\n")[0]}
+                  <br />
+                  {m.title.split("\\n")[1]}
+                </ContentTitle>
+                <ContentBoldTitle>작성일자 {m.createdAt}</ContentBoldTitle>
+              </div>
+            );
+          })}
         </FlexRowContainer>
       </Section>
 
@@ -134,9 +201,33 @@ const Home = () => {
 
       <Section>
         <SectionTitle isCenter={false}>Review</SectionTitle>
+        <FlexColumnContainer>
+          {/* review data call and rendering */}
+          {reviews.map((r) => {
+            // r.stay_id를 state에 있는 stay의 id와 비교해서 일치하면 stay의 name을 가져옴!
+            let r_stay;
+            stays.map((s) => {
+              if (s.id === r.stay_id) {
+                r_stay = s.name;
+              }
+            });
+            return (
+              <ReviewBox key={r.id}>
+                <ReviewPhoto bg={r.thumbnailUrl} />
+                <ReviewContent>
+                  <ContentTitle>{r.type}</ContentTitle>
+                  <ContentTitle>
+                    {r.date} / {r_stay}
+                  </ContentTitle>
+                  <ContentTitle>{r.summary}</ContentTitle>
+                </ReviewContent>
+              </ReviewBox>
+            );
+          })}
+        </FlexColumnContainer>
       </Section>
 
-      <Banner bg={photoUrl}>
+      <Banner bg={BannerPhotos.spaceBannerUrl}>
         <p>VIANDANTE STAY</p>
         <p>반려견과 함께하는 여행</p>
       </Banner>
@@ -144,16 +235,19 @@ const Home = () => {
       <Section>
         <SectionTitle isCenter={false}>Magazine</SectionTitle>
         <FlexRowContainer>
-          <div>
-            <BasicPhotoBox bg={photoUrl}></BasicPhotoBox>
-            <ContentTitle>"강아지와 차를 안전하게 타는 방법"</ContentTitle>
-            <ContentBoldTitle>작성일자 2022.01.10</ContentBoldTitle>
-          </div>
-          <div>
-            <BasicPhotoBox bg={photoUrl}></BasicPhotoBox>
-            <ContentTitle>"강아지와 물에서 노는 방법"</ContentTitle>
-            <ContentBoldTitle>작성일자 2022.01.01</ContentBoldTitle>
-          </div>
+          {magazines.map((m) => {
+            return (
+              <div key={m.id}>
+                <BasicPhotoBox bg={m.thumbnailUrl}></BasicPhotoBox>
+                <ContentTitle isCenter={false}>
+                  {m.title.split("\\n")[0]}
+                  <br />
+                  {m.title.split("\\n")[1]}
+                </ContentTitle>
+                <ContentBoldTitle>작성일자 {m.createdAt}</ContentBoldTitle>
+              </div>
+            );
+          })}
         </FlexRowContainer>
       </Section>
     </CustomContainer>
